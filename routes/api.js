97ddb0baad0e56dd63e4100d5a5427dd864ad9a8
demonz69 <92373913+demonz69@users.noreply.github.com>
2025-04-23 16:45:54 +0545

    configure api and set .env

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,29 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.get('/api/convert', function (req, res) {
+    const input = req.query.input;
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+
+    if (initNum === 'invalid number' && initUnit === 'invalid unit') {
+      return res.status(400).send('invalid number and unit');
+    }
+    if (initNum === 'invalid number') return res.send('invalid number');
+    if (initUnit === 'invalid unit') return res.send('invalid unit');
+  
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+  
+    res.json({
+      initNum: initNum,
+      initUnit: initUnit,
+      returnNum: returnNum,
+      returnUnit: returnUnit,
+      string: string
+    });
+
+  });
 };
